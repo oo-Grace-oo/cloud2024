707a367cf5ac95f06cd1a884eb3ebf7660411e12
Grace <lxy1123598050@gmail.com>
2024-06-03 22:45:22 +0800

    1、添加swagger 2、优化更新时间、创建时间格式展示

--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
@@ -4,6 +4,8 @@ import cn.hutool.json.JSONUtil;
 import com.atguigu.cloud.entities.Pay;
 import com.atguigu.cloud.entities.dto.PayDTO;
 import com.atguigu.cloud.service.PayService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -21,12 +23,14 @@ import java.util.List;
  */
 @RestController
 @Slf4j
+@Tag(name = "支付微服务模块", description = "支付CRUD")
 public class PayController {
 
     @Resource
     private PayService payService;
 
     @PostMapping(value = "/pay/add")
+    @Operation(summary = "新增", description = "新增支付流水")
     public String addPay(@RequestBody Pay pay) {
         log.error("插入数据：{}", JSONUtil.toJsonStr(pay));
         int add = payService.add(pay);
@@ -34,11 +38,13 @@ public class PayController {
     }
 
     @DeleteMapping("/pay/del/{id}")
+    @Operation(summary = "删除", description = "删除支付流水")
     public Integer deletePay(@PathVariable("id") Integer id) {
         return payService.delete(id);
     }
 
     @PutMapping(value = "/pay/update")
+    @Operation(summary = "修改", description = "修改支付流水")
     public String updatePay(@RequestBody PayDTO payDTO) {
         Pay pay = new Pay();
         BeanUtils.copyProperties(payDTO, pay);
@@ -47,11 +53,13 @@ public class PayController {
     }
 
     @GetMapping(value = "/pay/get/{id}")
+    @Operation(summary = "查询", description = "按照id查询流水")
     public Pay getPay(@PathVariable("id") Integer id) {
         return payService.getById(id);
     }
 
     @GetMapping(value = "/pay/get/all")
+    @Operation(summary = "查询全部", description = "查询全部流水")
     public List<Pay> getAll() {
         return payService.getAll();
     }
