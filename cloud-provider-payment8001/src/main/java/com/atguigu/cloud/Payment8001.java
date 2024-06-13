5f64aeacddb15a0beb10017d4f9ef935d7bbd4ee
oo-Grace-oo <lxy1123598050@gmail.com>
2024-06-13 19:29:34 +0800

    添加consul服务发现

--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/Payment8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/Payment8001.java
@@ -2,6 +2,7 @@ package com.atguigu.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 
@@ -15,6 +16,7 @@ import tk.mybatis.spring.annotation.MapperScan;
  */
 @SpringBootApplication
 @MapperScan("com.atguigu.cloud.mapper")
+@EnableDiscoveryClient
 public class Payment8001 {
     public static void main(String[] args) {
         SpringApplication.run(Payment8001.class, args);
