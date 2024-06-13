b08cc48d7db2d93960fad4607fb91088a19f2e85
Grace <lxy1123598050@gmail.com>
2024-06-14 00:45:06 +0800

    解决Order80调用失败问题

--- cloud-api-commons/src/main/java/com/atguigu/cloud/config/RestTemplateConfig.java
+++ cloud-api-commons/src/main/java/com/atguigu/cloud/config/RestTemplateConfig.java
@@ -1,6 +1,7 @@
 package com.atguigu.cloud.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
     @ConditionalOnMissingBean(RestTemplate.class)
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
